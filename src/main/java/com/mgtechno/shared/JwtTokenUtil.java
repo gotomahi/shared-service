35b085208d636407e30c760e91ac4c5398ec1161
Mahendra <mahendra.gondi@gmail.com>
2020-03-21 08:26:39 +0000

    jwt util changes

---
+++ src/main/java/com/mgtechno/shared/JwtTokenUtil.java
@@ -0,0 +1,53 @@
+package com.mgtechno.shared;
+
+import org.json.JSONObject;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.RsaSigner;
+import org.springframework.util.StringUtils;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Base64;
+
+import static com.mgtechno.shared.SharedConstants.*;
+import static com.mgtechno.shared.SharedConstants.COLON;
+
+public class JwtTokenUtil {
+
+    public static boolean isTokenVerified(KeyPair keyPair, String authHeader){
+        String[] parts = authHeader.split(PERIOD_REGEX);
+        RsaSigner signer = new RsaSigner((RSAPrivateKey)keyPair.getPrivate());
+        String encodedToken = JwtHelper.encode(decode(parts[1]), signer).getEncoded();
+        boolean verified = parts[2].equals(encodedToken.split(PERIOD_REGEX)[2]);
+        return verified;
+    }
+
+    public static boolean isTokenExpired(String authHeader){
+        boolean expired = false;
+        String[] parts = authHeader.split(PERIOD_REGEX);
+        JSONObject payload = new JSONObject(decode(parts[1]));
+        expired = payload.getLong("exp") < (System.currentTimeMillis() / 1000);
+        return expired;
+    }
+
+    public static String getAuthorizeHeader(String authorization, String clientId, String secret) {
+        if (StringUtils.isEmpty(authorization)) {
+            authorization = BASIC + TAB_SPACE + Base64.getEncoder().encodeToString((clientId + COLON + secret).getBytes());
+        }
+        return authorization;
+    }
+
+    public static String decode(String encodedString) {
+        return new String(Base64.getUrlDecoder().decode(encodedString));
+    }
+
+    public static JSONObject getTokenPayload(String authHeader){
+        String[] parts = authHeader.split(PERIOD_REGEX);
+        JSONObject payload = new JSONObject(decode(parts[1]));
+        return payload;
+    }
+
+    public Long getUserId(String authHeader){
+        return getTokenPayload(authHeader).getLong("userId");
+    }
+}
--- src/main/java/com/mgtechno/shared/JwtValidateFilter.java
+++ src/main/java/com/mgtechno/shared/JwtValidateFilter.java
@@ -1,9 +1,6 @@
 package com.mgtechno.shared;
 
-import org.json.JSONObject;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -16,9 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.KeyPair;
-import java.security.interfaces.RSAPrivateKey;
-import java.util.Base64;
 
+import static com.mgtechno.shared.JwtTokenUtil.isTokenExpired;
+import static com.mgtechno.shared.JwtTokenUtil.isTokenVerified;
 import static com.mgtechno.shared.SharedConstants.*;
 
 public class JwtValidateFilter extends GenericFilterBean {
@@ -38,7 +35,7 @@ public class JwtValidateFilter extends GenericFilterBean {
         if (!StringUtils.isEmpty(authorization) && (authorization.startsWith(BASIC)
                 || (authorization.startsWith(BEARER) && hasValidToken(authorization)))) {
             filterChain.doFilter(request, servletResponse);
-        } else if(!isTokenVerified(authorization)){
+        } else if(!isTokenVerified(keyPair, authorization)){
             response.setStatus(441);
         }else if(isTokenExpired(authorization)){
             response.setStatus(442);
@@ -46,33 +43,6 @@ public class JwtValidateFilter extends GenericFilterBean {
     }
 
     private boolean hasValidToken(String authorization) {
-        return isTokenVerified(authorization) && !isTokenExpired(authorization);
-    }
-
-    private boolean isTokenVerified(String token){
-        String[] parts = token.split(PERIOD_REGEX);
-        RsaSigner signer = new RsaSigner((RSAPrivateKey)this.keyPair.getPrivate());
-        String encodedToken = JwtHelper.encode(decode(parts[1]), signer).getEncoded();
-        boolean verified = parts[2].equals(encodedToken.split(PERIOD_REGEX)[2]);
-        return verified;
-    }
-
-    private boolean isTokenExpired(String token){
-        boolean expired = false;
-        String[] parts = token.split(PERIOD_REGEX);
-        JSONObject payload = new JSONObject(decode(parts[1]));
-        expired = payload.getLong("exp") < (System.currentTimeMillis() / 1000);
-        return expired;
-    }
-
-    private String getAuthorizeHeader(String authorization, String clientId, String secret) {
-        if (StringUtils.isEmpty(authorization)) {
-            authorization = BASIC + TAB_SPACE + Base64.getEncoder().encodeToString((clientId + COLON + secret).getBytes());
-        }
-        return authorization;
-    }
-
-    private static String decode(String encodedString) {
-        return new String(Base64.getUrlDecoder().decode(encodedString));
+        return isTokenVerified(keyPair, authorization) && !isTokenExpired(authorization);
     }
 }
