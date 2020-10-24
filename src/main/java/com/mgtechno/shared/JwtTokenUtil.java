eafb95e26ad6c702d29a7cc5be11516b7c01406a
Mahendra <mahendra.gondi@gmail.com>
2020-10-24 06:42:14 +0100

    removed spring dependency

--- src/main/java/com/mgtechno/shared/JwtTokenUtil.java
+++ src/main/java/com/mgtechno/shared/JwtTokenUtil.java
@@ -1,26 +1,13 @@
 package com.mgtechno.shared;
 
+import org.apache.commons.lang.StringUtils;
 import org.json.JSONObject;
-import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.jwt.crypto.sign.RsaSigner;
-import org.springframework.util.StringUtils;
 
-import java.security.KeyPair;
-import java.security.interfaces.RSAPrivateKey;
 import java.util.Base64;
 
 import static com.mgtechno.shared.SharedConstants.*;
-import static com.mgtechno.shared.SharedConstants.COLON;
 
 public class JwtTokenUtil {
-
-    public static boolean isTokenVerified(KeyPair keyPair, String authHeader){
-        String[] parts = authHeader.split(PERIOD_REGEX);
-        RsaSigner signer = new RsaSigner((RSAPrivateKey)keyPair.getPrivate());
-        String encodedToken = JwtHelper.encode(decode(parts[1]), signer).getEncoded();
-        boolean verified = parts[2].equals(encodedToken.split(PERIOD_REGEX)[2]);
-        return verified;
-    }
 
     public static boolean isTokenExpired(String authHeader){
         boolean expired = false;
